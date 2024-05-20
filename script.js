348df831195106707fdafdf93904c54f4f3af975
Light <149325277+lightloss-real@users.noreply.github.com>
2024-05-20 08:54:46 -0500

    GOD i love working on websites

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ document.getElementById('navbar-toggle').addEventListener('click', function() {
     menu.classList.toggle('active');
 });
 
+function orderItem(itemName) {
+    alert(`You've ordered a ${itemName}!`);
+}
+
 //This creates the nice menu bar you see on top. Really classy, if I say so myself.//
--- script2.js
+++
@@ -1,5 +0,0 @@
-function orderItem(itemName) {
-    alert(`You've ordered a ${itemName}!`);
-}
-
-//This makes an alert pop up when you click the "Order" button.//
